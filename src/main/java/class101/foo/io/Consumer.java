78541b0c0e1dff7a0b104d4e86d7d4d95b16527f
sui <numerical43@daum.net>
2023-01-26 16:22:50 +0900

    Feat : Consumer 추가

--- src/main/java/class101/foo/io/Consumer.java
+++ src/main/java/class101/foo/io/Consumer.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Comsumer {
+public class Consumer {
 
     @Autowired
     ObjectMapper objectMapper;
